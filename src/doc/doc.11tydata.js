e722397dc26df839f6250511c01a8d8fa4566519
Bruce Hoppe <Bruce Hoppe>
2022-09-09 16:07:47 -0400

    draft articles

--- src/doc/doc.11tydata.js
+++ src/doc/doc.11tydata.js
@@ -1,9 +1,54 @@
-module.exports = {
-    eleventyComputed: {
+require('dotenv').config();
+
+const isDevEnv = process.env.ELEVENTY_ENV === 'development';
+const todaysDate = new Date();
+
+function showDraft(data) {
+	const isDraft = 'draft' in data && data.draft !== false;
+	const isFutureDate = data.page.date > todaysDate;
+	return isDevEnv || (!isDraft && !isFutureDate);
+}
+
+module.exports = function() {
+	return {
+		eleventyComputed: {
+			eleventyExcludeFromCollections: function(data) {
+				if(showDraft(data)) {
+					return data.eleventyExcludeFromCollections;
+				}
+				else {
+					return true;
+				}
+			},
+			permalink: function(data) {
+				if(showDraft(data)) {
+					return data.permalink
+				}
+				else {
+					return false;
+				}
+			},
       eleventyNavigation: {
-        key: data => data.title
+        key: function(data) {   
+          if(showDraft(data)) {
+            return data.title
+          }
+          else {
+            return false;
+          }
+        }
       },
-      sidebar: 'toc',
-      background: 'bg-white'
-    }
-  };
+      sidebar: function(data) {
+        return 'toc';
+      },
+      background: function(data) {
+				if(('draft' in data && data.draft !== false) || (data.page.date > todaysDate)) {
+					return 'text-white bg-info'
+				}
+				else {
+					return 'bg-white';
+        }
+      }
+		}
+	}
+}
